51869d6b4526f621a7622d65ea48b42068d88817
JazminAyala <tarin_ayala@hotmail.com>
2018-01-23 10:36:22 -0600

    subiendo repo

---
+++ js/splash.js
@@ -0,0 +1,10 @@
+/*   vista splasch  */
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#content").fadeOut(1500);
+    },3000);
+	
+	setTimeout(function() {
+        $("#content2").fadeIn(1500);
+    },6000);
+});
